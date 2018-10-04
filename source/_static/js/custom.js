c9b26a7f86c4b015dc019a7e749fb748e785a662
dgarcia360 <dgarcia360@outlook.com>
2018-10-04 11:16:39 +0100

    Added translation button to footer

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -11,9 +11,43 @@ function addSidebarClickHandler(){
     });
 }
 
+const translations = ['ja'];
+
+
+function translatePathRedirect(languageCode) {
+    const path = window.location.pathname;
+    let splitPath = path.split('/');
+    if (languageCode === 'en') {
+        splitPath.shift();
+        splitPath.shift();
+    }
+    else if(splitPath[1] in translations) {
+        splitPath[1] = languageCode;
+    }
+    else{
+        splitPath.splice(1, 0, languageCode);
+    }
+    window.location.href = splitPath.join('/');
+}
+
+function initLanguageSelector(){
+    const languageSelector = $('#language select');
+    const path = window.location.pathname;
+    const languageCode = path.split('/')[1];
+    if (languageCode in translations) {
+         languageSelector.val( languageCode );
+    }
+    else languageSelector.val( 'en' );
+
+    languageSelector.change(function () {
+        translatePathRedirect($(this).val());
+    });
+}
+
 $( document ).ready(function() {
     homePageFullWidth();
     addSidebarClickHandler();
+    initLanguageSelector();
 });
 
 
