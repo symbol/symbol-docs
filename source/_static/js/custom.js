6aa2c2a4342b56ad63b6461582e80e2818267cfb
dgarcia360 <dgarcia360@outlook.com>
2019-06-09 08:14:28 +0100

    Fixed lint

--- source/_static/js/custom.js
+++ source/_static/js/custom.js
@@ -3,12 +3,12 @@ const translations = ['ja', 'zh_CN'];
 function translatePathRedirect(languageCode) {
     const path = window.location.pathname;
     let splitPath = path.split('/');
-    if (splitPath[1] !== languageCode){
+    if (splitPath[1] !== languageCode) {
         if (languageCode === 'en') {
             splitPath.splice(1, 1);
         } else if (translations.indexOf(splitPath[1]) > -1) {
             splitPath[1] = languageCode;
-        } else if(translations.indexOf(languageCode) > -1){
+        } else if (translations.indexOf(languageCode) > -1) {
             splitPath.splice(1, 0, languageCode);
         }
         window.location.href = splitPath.join('/');
@@ -25,40 +25,40 @@ function initLanguageSelector() {
         languageSelector.val('en');
     }
     languageSelector.change(function () {
-        if($(this).val() !== 'translate'){
+        if ($(this).val() !== 'translate') {
             translatePathRedirect($(this).val());
         }
-        else{
+        else {
             window.location.href = 'https://www.transifex.com/nemtech/nem2docs/';
         }
     });
 }
 
-function setPreferredCodeTab(){
-    try{
+function setPreferredCodeTab() {
+    try {
         let urlParams = new URLSearchParams(window.location.search);
         if (urlParams.has('code')) {
             localStorage.setItem('preferredCodeTab', urlParams.get('code'));
         }
-    } catch(e){
+    } catch (e) {
         console.log("Error:" + e);
     }
 }
 
-function clickPreferredCodeTab(){
+function clickPreferredCodeTab() {
     const code = localStorage.getItem('preferredCodeTab');
-    if (code !== null){
-        if (code === 'typescript'){
+    if (code !== null) {
+        if (code === 'typescript') {
             $("[data-tab=tab-0-0]").click();
-        } else if (code === 'javascript'){
+        } else if (code === 'javascript') {
             $("[data-tab=tab-0-1]").click();
         }
         $(".highlight-" + code).click();
     }
 }
 
-function addBlockCaptionInsideCodeExample(){
-    $(".code-block-caption").each(function() {
+function addBlockCaptionInsideCodeExample() {
+    $(".code-block-caption").each(function () {
         $(this).next().prepend(this);
     });
     $(".literal-block-wrapper").removeClass("container");
@@ -66,13 +66,13 @@ function addBlockCaptionInsideCodeExample(){
 
 }
 
-$( document ).ready(function() {
+$(document).ready(function () {
     addBlockCaptionInsideCodeExample();
     initLanguageSelector();
     setPreferredCodeTab();
-    $('a.external').attr("target","_blank");
+    $('a.external').attr("target", "_blank");
 });
 
-$(window).on('load', function(){
+$(window).on('load', function () {
     clickPreferredCodeTab();
 });
