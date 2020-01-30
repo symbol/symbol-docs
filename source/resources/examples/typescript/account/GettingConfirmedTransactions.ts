777ab53c59563e030345c048d37a55e075159b9a
dgarcia360 <dgarcia360@outlook.com>
2020-01-30 16:28:48 +0000

    Fixed example

--- source/resources/examples/typescript/account/GettingConfirmedTransactions.ts
+++ source/resources/examples/typescript/account/GettingConfirmedTransactions.ts
@@ -27,8 +27,8 @@ const address = Address.createFromRawAddress(rawAddress);
 const nodeUrl = 'http://api-xym-harvest-20.us-west-1.nemtech.network:3000';
 const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
 const accountHttp = repositoryFactory.createAccountRepository();
-
-const pageSize = 10; GettingConfirmedTransactions// Page size between 10 and 100, otherwise 10
+// Page size between 10 and 100
+const pageSize = 10;
 
 accountHttp
     .getAccountTransactions(address, new QueryParams(pageSize))
