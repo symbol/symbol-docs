f13f271cb9ef86591d959f7e9622cbac645bbadd
Ivy Fung <40448458+ivyfung1@users.noreply.github.com>
2020-11-04 00:13:32 +0800

    Added 2 transaction files
    ActivatingDelegatedHarvestingVrfKeyLink.ts
    ActivatingDelegatedHarvestingNodeKeyLink.ts

---
+++ source/resources/examples/typescript/accountlink/ActivatingDelegatedHarvestingNodeKeyLink.ts
@@ -0,0 +1,43 @@
+import {
+    Account,
+    Deadline,
+    LinkAction,
+    NetworkType,
+    NodeKeyLinkTransaction,
+    PublicAccount,
+    RepositoryFactoryHttp,
+    UInt64
+} from 'symbol-sdk';
+
+/* start block 01 */
+// Set network type
+const networkType = NetworkType.TEST_NET;
+// Main account private key for signing transaction
+const mainAccountPrivateKey = '0000000000000000000000000000000000000000000000000000000000000000';
+const mainAccount = Account.createFromPrivateKey(mainAccountPrivateKey, networkType);
+// Node account
+const nodePublicKey = '70E06C112848A652D635755B7530D3096A978321D09B8D8DC17505CAE09565C5';
+const nodeAccount = PublicAccount.createFromPublicKey(nodePublicKey, networkType);
+/* end block 01 */
+
+/* start block 02 */
+const  nodeLinkTransaction = NodeKeyLinkTransaction.create(
+    Deadline.create(),
+    nodeAccount.publicKey,
+    LinkAction.Link,
+    networkType,
+    UInt64.fromUint(2000000)); // Absolute number
+/* end block 02 */
+
+/* start block 03 */
+// Replace with any node in the network
+const nodeUrl = 'http://api-01.ap-northeast-1.0.10.0.x.symboldev.network:3000';
+// Replace with networkGenerationHashSeed by querying http://<node-url>:3000/node/info
+const networkGenerationHash = '6C1B92391CCB41C96478471C2634C111D9E989DECD66130C0430B5B8D20117CD';
+const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
+const transactionHttp = repositoryFactory.createTransactionRepository();
+const signedTransaction = mainAccount.sign(nodeLinkTransaction, networkGenerationHash);
+transactionHttp
+    .announce(signedTransaction)
+    .subscribe((x) => console.log(x), (err) => console.error(err));
+/* end block 03 */
---
+++ source/resources/examples/typescript/accountlink/ActivatingDelegatedHarvestingVrfKeyLink.ts
@@ -0,0 +1,41 @@
+import {
+    Account,
+    Deadline,
+    LinkAction,
+    NetworkType,
+    RepositoryFactoryHttp,
+    UInt64,
+    VrfKeyLinkTransaction
+} from 'symbol-sdk';
+
+/* start block 01 */
+// Set network type
+const networkType = NetworkType.TEST_NET;
+// Main account private key for signing transaction
+const mainAccountPrivateKey = '0000000000000000000000000000000000000000000000000000000000000000';
+const mainAccount = Account.createFromPrivateKey(mainAccountPrivateKey, networkType);
+// Generate a new account as vrf account
+const vrfAccount = Account.generateNewAccount(networkType);
+/* end block 01 */
+
+/* start block 02 */
+const vrfLinkTransaction = VrfKeyLinkTransaction.create(
+    Deadline.create(),
+    vrfAccount.publicKey,
+    LinkAction.Link,
+    networkType,
+    UInt64.fromUint(2000000)); // Absolute number
+/* end block 02 */
+
+/* start block 03 */
+// Replace with any node in the network
+const nodeUrl = 'http://api-01.ap-northeast-1.0.10.0.x.symboldev.network:3000';
+// Replace with networkGenerationHashSeed by querying http://<node-url>:3000/node/info
+const networkGenerationHash = '6C1B92391CCB41C96478471C2634C111D9E989DECD66130C0430B5B8D20117CD';
+const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
+const transactionHttp = repositoryFactory.createTransactionRepository();
+const signedTransaction = mainAccount.sign(vrfLinkTransaction, networkGenerationHash);
+transactionHttp
+    .announce(signedTransaction)
+    .subscribe((x) => console.log(x), (err) => console.error(err));
+/* end block 03 */
