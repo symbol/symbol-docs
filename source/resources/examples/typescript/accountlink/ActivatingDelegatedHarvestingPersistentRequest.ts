fe32c33b297d615ca6bf9efc16645517c09366f5
Ivy Fung <40448458+ivyfung1@users.noreply.github.com>
2020-11-04 10:48:44 +0800

    Update ActivatingDelegatedHarvestingPersistentRequest.ts
    Update with new procedures

--- source/resources/examples/typescript/accountlink/ActivatingDelegatedHarvestingPersistentRequest.ts
+++ source/resources/examples/typescript/accountlink/ActivatingDelegatedHarvestingPersistentRequest.ts
@@ -9,40 +9,46 @@ import {
 } from 'symbol-sdk';
 
 /* start block 01 */
-// replace with network type
+// Set network type
 const networkType = NetworkType.TEST_NET;
-// replace with private key
+// Main account private key
 const mainAccountPrivateKey = '0000000000000000000000000000000000000000000000000000000000000000';
 const mainAccount = Account.createFromPrivateKey(mainAccountPrivateKey, networkType);
-const remoteAccount = Account.generateNewAccount(networkType);
-// replace with private key
+// Remote account private key
+const remoteAccountPrivateKey = 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+const remoteAccount = Account.createFromPrivateKey(remoteAccountPrivateKey, networkType);
+// Vrf account private key
+const vrfAccountPrivateKey = 'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB';
+const vrfAccount = Account.createFromPrivateKey(vrfAccountPrivateKey, networkType);
+// Announcer private key
 const announcerAccountPrivateKey = 'FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF';
 const announcerAccount = Account.createFromPrivateKey(announcerAccountPrivateKey, networkType);
+// Node account - Replace with publicKey by querying http://<node-url>:3000/node/info
+const nodePublicKey = '46DDDBCC4DB446BA1BEEF0294B51327BD2872625A235E658E3D1111F45FAD25D';
+const nodeAccount = PublicAccount.createFromPublicKey(nodePublicKey, networkType);
 /* end block 01 */
 
 /* start block 02 */
-// replace with node publicKey (nodeUrl + '/node/info')
-const nodePublicKey = '3A537D5A1AF51158C42F80A199BB58351DBF3253C4A6A1B7BD1014682FB595EA';
-const nodePublicAccount = PublicAccount.createFromPublicKey(nodePublicKey, networkType);
-
 const persistentDelegationRequestTransaction = PersistentDelegationRequestTransaction
     .createPersistentDelegationRequestTransaction(
         Deadline.create(),
         remoteAccount.privateKey,
-        nodePublicAccount.publicKey,
+        vrfAccount.privateKey,
+        nodeAccount.publicKey,
         networkType,
         UInt64.fromUint(2000000));
 /* end block 02 */
 
 /* start block 03 */
-// replace with meta.networkGenerationHash (nodeUrl + '/node/info')
-const networkGenerationHash = '1DFB2FAA9E7F054168B0C5FCB84F4DEB62CC2B4D317D861F3168D161F54EA78B';
-const signedTransaction = announcerAccount.sign(persistentDelegationRequestTransaction, networkGenerationHash);
-
-// replace with node endpoint
-const nodeUrl = 'http://api-01.us-east-1.096x.symboldev.network:3000';
+// Replace with networkGenerationHashSeed by querying http://<node-url>:3000/node/info
+const networkGenerationHash = '6C1B92391CCB41C96478471C2634C111D9E989DECD66130C0430B5B8D20117CD';
+// Replace with any node in the network
+const nodeUrl = 'http://api-01.ap-northeast-1.0.10.0.x.symboldev.network:3000';
 const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
 const transactionHttp = repositoryFactory.createTransactionRepository();
+const signedTransaction = announcerAccount.sign(persistentDelegationRequestTransaction, networkGenerationHash);
+console.log('Transaction hash:', signedTransaction.hash);
+
 transactionHttp
     .announce(signedTransaction)
     .subscribe((x) => console.log(x), (err) => console.error(err));
