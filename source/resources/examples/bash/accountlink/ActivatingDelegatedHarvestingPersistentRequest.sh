ed4f1d2fe8607b86146c82fefd8380fd5dbacf24
Ivy Fung <40448458+ivyfung1@users.noreply.github.com>
2020-11-04 16:23:10 +0800

    Update ActivatingDelegatedHarvestingPersistentRequest.sh
    Added --sync

--- source/resources/examples/bash/accountlink/ActivatingDelegatedHarvestingPersistentRequest.sh
+++ source/resources/examples/bash/accountlink/ActivatingDelegatedHarvestingPersistentRequest.sh
@@ -1,3 +1,3 @@
  #!/bin/sh
 
-symbol-cli transaction persistentharvestdelegation --remote-private-key <REMOTE_PRIVATE_KEY> --recipient-public-key <NODE_PUBLIC_KEY> --vrf-private-key <VRF_PRIVATE_KEY> --profile announcer
+symbol-cli transaction persistentharvestdelegation --remote-private-key <REMOTE_PRIVATE_KEY> --recipient-public-key <NODE_PUBLIC_KEY> --vrf-private-key <VRF_PRIVATE_KEY> --profile announcer --sync
