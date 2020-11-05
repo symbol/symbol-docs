9cf005699a2980c86e6a338e4028cc1f7cc6d223
Xavi Artigas <xavierartigas@yahoo.es>
2020-11-05 16:56:54 +0100

    Added Delegated Harvesting Concept
    Also, formatting of the guide and source code.

--- source/resources/examples/bash/accountlink/ActivatingDelegatedHarvestingVrfKeyLink.sh
+++ source/resources/examples/bash/accountlink/ActivatingDelegatedHarvestingVrfKeyLink.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-symbol-cli transaction vrfkeylink --linked-public-key <VRF_PUBLIC_KEY> --action Link --profile main --sync
+symbol-cli transaction vrfkeylink \
+    --linked-public-key <VRF_PUBLIC_KEY> \
+    --action Link \
+    --sync
