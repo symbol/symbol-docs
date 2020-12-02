232f8e4097956abf4278db77e49100bc78daf327
fernando <fboucquez@gmail.com>
2020-12-02 07:30:14 -0300

    Updated transfer transaction examples Using async await instead of observable's methods for simplicity Removed hardcoded deadline, network type, generation hash, currency information Added eslint and prettier (but not exectued yet)

--- source/resources/examples/typescript/transfer/SendingATransferTransaction.ts
+++ source/resources/examples/typescript/transfer/SendingATransferTransaction.ts
@@ -16,61 +16,47 @@
  *
  */
 
-import {
-    Account,
-    Address,
-    Deadline,
-    Mosaic,
-    MosaicId,
-    NetworkType,
-    PlainMessage,
-    RepositoryFactoryHttp,
-    TransferTransaction,
-    UInt64,
-} from 'symbol-sdk';
+import { Account, Address, Deadline, PlainMessage, RepositoryFactoryHttp, TransferTransaction, UInt64 } from 'symbol-sdk';
 
-// Retrieve from node's /network/properties or RepositoryFactory
-const epochAdjustment = 123456789;
+const example = async () => {
+  //Network information
+  const nodeUrl = 'http://api-01.us-east-1.0.10.0.x.symboldev.network:3000';
+  const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
+  const epochAdjustment = await repositoryFactory.getEpochAdjustment().toPromise();
+  const networkType = await repositoryFactory.getNetworkType().toPromise();
 
-/* start block 01 */
-// replace with recipient address
-const rawAddress = 'TB6Q5E-YACWBP-CXKGIL-I6XWCH-DRFLTB-KUK34I-YJQ';
-const recipientAddress = Address.createFromRawAddress(rawAddress);
-// replace with network type
-const networkType = NetworkType.TEST_NET;
-// replace with symbol.xym id
-const networkCurrencyMosaicId = new MosaicId('5E62990DCAC5BE8A');
-// replace with network currency divisibility
-const networkCurrencyDivisibility = 6;
+  /* start block 01 */
+  // replace with recipient address
+  const rawAddress = 'TB6Q5E-YACWBP-CXKGIL-I6XWCH-DRFLTB-KUK34I-YJQ';
+  const recipientAddress = Address.createFromRawAddress(rawAddress);
+  // returns the network main currency, symbol.xym
+  const { currency } = await repositoryFactory.getCurrencies().toPromise();
 
-const transferTransaction = TransferTransaction.create(
+  const transferTransaction = TransferTransaction.create(
     Deadline.create(epochAdjustment),
     recipientAddress,
-    [new Mosaic (networkCurrencyMosaicId,
-        UInt64.fromUint(10 * Math.pow(10, networkCurrencyDivisibility)))],
+    [currency.createAbsolute(10)],
     PlainMessage.create('This is a test message'),
     networkType,
-    UInt64.fromUint(2000000));
-/* end block 01 */
+    UInt64.fromUint(2000000),
+  );
+  /* end block 01 */
 
-/* start block 02 */
-// replace with sender private key
-const privateKey = '1111111111111111111111111111111111111111111111111111111111111111';
-const account = Account.createFromPrivateKey(privateKey, networkType);
-// replace with meta.networkGenerationHash (nodeUrl + '/node/info')
-const networkGenerationHash = '1DFB2FAA9E7F054168B0C5FCB84F4DEB62CC2B4D317D861F3168D161F54EA78B';
-const signedTransaction = account.sign(transferTransaction, networkGenerationHash);
-console.log('Payload:', signedTransaction.payload);
-console.log('Transaction Hash:', signedTransaction.hash);
-/* end block 02 */
+  /* start block 02 */
+  // replace with sender private key
+  const privateKey = '1111111111111111111111111111111111111111111111111111111111111111';
+  const account = Account.createFromPrivateKey(privateKey, networkType);
+  const networkGenerationHash = await repositoryFactory.getGenerationHash().toPromise();
+  const signedTransaction = account.sign(transferTransaction, networkGenerationHash);
+  console.log('Payload:', signedTransaction.payload);
+  console.log('Transaction Hash:', signedTransaction.hash);
+  /* end block 02 */
 
-/* start block 03 */
-// replace with node endpoint
-const nodeUrl = 'http://api-01.us-east-1.0.10.0.x.symboldev.network:3000';
-const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
-const transactionHttp = repositoryFactory.createTransactionRepository();
+  /* start block 03 */
+  // replace with node endpoint
+  const transactionRepository = repositoryFactory.createTransactionRepository();
 
-transactionHttp
-    .announce(signedTransaction)
-    .subscribe((x) => console.log(x), (err) => console.error(err));
-/* end block 03 */
+  const response = transactionRepository.announce(signedTransaction).toPromise();
+  console.log(response);
+  /* end block 03 */
+};
