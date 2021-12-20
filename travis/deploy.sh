bd668f2b410d845623fed4ee801660dc0ba55369
Xavi Artigas <xavierartigas@yahoo.es>
2021-12-20 16:28:48 +0100

    feat: Push compiled site to a folder instead of another repo

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,30 +4,28 @@ set -e
 echo "Build docs..." && echo -en "travis_fold:start:Build.AllDocs\\r"
 make alldocs
 echo -en "travis_fold:end:Build.AllDocs\\r"
-echo "Link Check..." && echo -en "travis_fold:start:Link.Check"
+
+echo "Link Check..." && echo -en "travis_fold:start:Link.Check\\r"
 make linkcheck
 echo -en "travis_fold:end:Link.Check\\r"
-cp CNAME build/html
-
-echo "Moving to build folder"
-cd build/html
-
-echo "Setting remote url https://github.com/${RELEASE_REPO_SLUG}.git"
-git init
-git config user.name "symbol-bot"
-git config user.email "${GITHUB_EMAIL}"
-git remote add origin "https://${GITHUB_TOKEN}@github.com/${RELEASE_REPO_SLUG}.git" > /dev/null 2>&1
 
 #  The $SKIP_RELEASE_PUBLISH env variable can avoid republishing if the release process fails.
 if [ "$SKIP_RELEASE_PUBLISH" = "true" ]; then
    echo "Skipping publishing of docs"
-   echo ""
 else
-   echo "Publishing docs"
-   git add .
-   echo "Commit"
+   echo "Publishing..." && echo -en "travis_fold:start:Publish\\r"
+
+   git config user.name "symbol-bot"
+   git config user.email "${GITHUB_EMAIL}"
+   git remote add github "https://${GITHUB_TOKEN}@github.com/symbol/symbol-docs.git" > /dev/null 2>&1
+
+   git checkout -f gh-pages
+   git reset --hard main
+
+   mv -f -t docs build/html/*
+
+   git add -A docs
    git commit -m "Release docs" > /dev/null 2>&1
-   echo "Push"
-   git push --set-upstream origin master --force
-echo ""
+   git push --set-upstream github gh-pages --force
+   echo -en "travis_fold:end:Link.Publish\\r"
 fi
