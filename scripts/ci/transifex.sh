766eee521d11337a6e3336fe33f97f5b93df0b1f
wayonb <wayon@symbol.dev>
2022-02-24 12:06:14 -0500

    [jenkins] fix: address feedback

--- scripts/ci/transifex.sh
+++ scripts/ci/transifex.sh
@@ -2,24 +2,14 @@
 
 set -ex
 
-tx_install() {
-    pip install transifex-client
-}
+python -m pip install transifex-client
 
-tx_init() {
-    echo "[https://www.transifex.com]
+echo "[https://www.transifex.com]
 hostname = https://www.transifex.com
 username = ${TRANSIFEX_USER}
 password = ${TRANSIFEX_PASSWORD}
 " > ~/.transifexrc
-}
-
-tx_push() {
-    tx push --source --parallel
-}
 
 echo "Publishing to Transifex..."
-tx_install
-tx_init
-tx_push
+tx push --source --parallel
 echo "Finish pushing to Transifex"
